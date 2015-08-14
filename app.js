3427532d948f221d85ca205c9478e2b434f34091
trebek1 <trebek08@gmail.com>
2015-08-13 20:22:21 -0700

    added tabset directive

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-;(function(window) {
-
 angular.module('app', [])
+.directive('tab', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+    scope: { },
+    link: function(scope, elem, attr) {}
+  }
+})
 
-// Define directives here
-
-})(window);
+.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { },
+    templateUrl: 'tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function() {
+      var self = this
+      self.tabs = []
+    }
+  }
+})
